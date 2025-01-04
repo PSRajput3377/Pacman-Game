ba168df198d8fd72c222ee8078e78499377ca364
PSRajput3377 <prashantrathou63@gmail.com>
2025-01-04 23:20:12 +0530

    more efficient

--- search.py
+++ search.py
@@ -1,117 +1,300 @@
-# search.py
-# ---------
-# Licensing Information: Please do not distribute or publish solutions to this
-# project. You are free to use and extend these projects for educational
-# purposes. The Pacman AI projects were developed at UC Berkeley, primarily by
-# John DeNero (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
 
-"""
-In search.py, you will implement generic search algorithms which are called 
-by Pacman agents (in searchAgents.py).
-"""
+
+# """
+# In search.py, you will implement generic search algorithms which are called 
+# by Pacman agents (in searchAgents.py).
+# """
+
+# import util
+
+# class SearchProblem:
+#   """
+#   This class outlines the structure of a search problem, but doesn't implement
+#   any of the methods (in object-oriented terminology: an abstract class).
+  
+#   You do not need to change anything in this class, ever.
+#   """
+  
+#   def getStartState(self):
+#      """
+#      Returns the start state for the search problem 
+#      """
+#      util.raiseNotDefined()
+    
+#   def isGoalState(self, state):
+#      """
+#        state: Search state
+    
+#      Returns True if and only if the state is a valid goal state
+#      """
+#      util.raiseNotDefined()
+
+#   def getSuccessors(self, state):
+#      """
+#        state: Search state
+     
+#      For a given state, this should return a list of triples, 
+#      (successor, action, stepCost), where 'successor' is a 
+#      successor to the current state, 'action' is the action
+#      required to get there, and 'stepCost' is the incremental 
+#      cost of expanding to that successor
+#      """
+#      util.raiseNotDefined()
+
+#   def getCostOfActions(self, actions):
+#      """
+#       actions: A list of actions to take
+ 
+#      This method returns the total cost of a particular sequence of actions.  The sequence must
+#      be composed of legal moves
+#      """
+#      util.raiseNotDefined()
+           
+
+# def tinyMazeSearch(problem):
+#   """
+#   Returns a sequence of moves that solves tinyMaze.  For any other
+#   maze, the sequence of moves will be incorrect, so only use this for tinyMaze
+#   """
+#   from game import Directions
+#   s = Directions.SOUTH
+#   w = Directions.WEST
+#   return  [s,s,w,s,w,w,s,w]
+
+# def depthFirstSearch(problem):
+#   """
+#   Search the deepest nodes in the search tree first
+#   [2nd Edition: p 75, 3rd Edition: p 87]
+  
+#   Your search algorithm needs to return a list of actions that reaches
+#   the goal.  Make sure to implement a graph search algorithm 
+#   [2nd Edition: Fig. 3.18, 3rd Edition: Fig 3.7].
+  
+#   To get started, you might want to try some of these simple commands to
+#   understand the search problem that is being passed in:
+  
+#   print "Start:", problem.getStartState()
+#   print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+#   print "Start's successors:", problem.getSuccessors(problem.getStartState())
+#   """
+#   "*** YOUR CODE HERE ***"
+#   util.raiseNotDefined()
+
+# def breadthFirstSearch(problem):
+#   """
+#   Search the shallowest nodes in the search tree first.
+#   [2nd Edition: p 73, 3rd Edition: p 82]
+#   """
+#   "*** YOUR CODE HERE ***"
+#   util.raiseNotDefined()
+      
+# def uniformCostSearch(problem):
+#   "Search the node of least total cost first. "
+#   "*** YOUR CODE HERE ***"
+#   util.raiseNotDefined()
+
+# def nullHeuristic(state, problem=None):
+#   """
+#   A heuristic function estimates the cost from the current state to the nearest
+#   goal in the provided SearchProblem.  This heuristic is trivial.
+#   """
+#   return 0
+
+# def aStarSearch(problem, heuristic=nullHeuristic):
+#   "Search the node that has the lowest combined cost and heuristic first."
+#   "*** YOUR CODE HERE ***"
+#   util.raiseNotDefined()
+    
+  
+# # Abbreviations
+# bfs = breadthFirstSearch
+# dfs = depthFirstSearch
+# astar = aStarSearch
+# ucs = uniformCostSearch
+
+
+
+
+
+
+# new search algorithm for pacman
 
 import util
 
 class SearchProblem:
-  """
-  This class outlines the structure of a search problem, but doesn't implement
-  any of the methods (in object-oriented terminology: an abstract class).
-  
-  You do not need to change anything in this class, ever.
-  """
-  
-  def getStartState(self):
-     """
-     Returns the start state for the search problem 
-     """
-     util.raiseNotDefined()
-    
-  def isGoalState(self, state):
-     """
-       state: Search state
-    
-     Returns True if and only if the state is a valid goal state
-     """
-     util.raiseNotDefined()
+    """
+    This class outlines the structure of a search problem, but doesn't implement
+    any of the methods (in object-oriented terminology: an abstract class).
+    """
 
-  def getSuccessors(self, state):
-     """
-       state: Search state
-     
-     For a given state, this should return a list of triples, 
-     (successor, action, stepCost), where 'successor' is a 
-     successor to the current state, 'action' is the action
-     required to get there, and 'stepCost' is the incremental 
-     cost of expanding to that successor
-     """
-     util.raiseNotDefined()
+    def getStartState(self):
+        """
+        Returns the start state for the search problem 
+        """
+        util.raiseNotDefined()
 
-  def getCostOfActions(self, actions):
-     """
-      actions: A list of actions to take
- 
-     This method returns the total cost of a particular sequence of actions.  The sequence must
-     be composed of legal moves
-     """
-     util.raiseNotDefined()
-           
+    def isGoalState(self, state):
+        """
+        state: Search state
+
+        Returns True if and only if the state is a valid goal state
+        """
+        util.raiseNotDefined()
+
+    def getSuccessors(self, state):
+        """
+        state: Search state
+
+        For a given state, this should return a list of triples, 
+        (successor, action, stepCost), where 'successor' is a 
+        successor to the current state, 'action' is the action
+        required to get there, and 'stepCost' is the incremental 
+        cost of expanding to that successor
+        """
+        util.raiseNotDefined()
+
+    def getCostOfActions(self, actions):
+        """
+        actions: A list of actions to take
+
+        This method returns the total cost of a particular sequence of actions.  The sequence must
+        be composed of legal moves
+        """
+        util.raiseNotDefined()
+
 
 def tinyMazeSearch(problem):
-  """
-  Returns a sequence of moves that solves tinyMaze.  For any other
-  maze, the sequence of moves will be incorrect, so only use this for tinyMaze
-  """
-  from game import Directions
-  s = Directions.SOUTH
-  w = Directions.WEST
-  return  [s,s,w,s,w,w,s,w]
+    """
+    Returns a sequence of moves that solves tinyMaze.  For any other
+    maze, the sequence of moves will be incorrect, so only use this for tinyMaze
+    """
+    from game import Directions
+    return [Directions.SOUTH, Directions.SOUTH, Directions.WEST, Directions.SOUTH,
+            Directions.WEST, Directions.WEST, Directions.SOUTH, Directions.WEST]
+
 
 def depthFirstSearch(problem):
-  """
-  Search the deepest nodes in the search tree first
-  [2nd Edition: p 75, 3rd Edition: p 87]
-  
-  Your search algorithm needs to return a list of actions that reaches
-  the goal.  Make sure to implement a graph search algorithm 
-  [2nd Edition: Fig. 3.18, 3rd Edition: Fig 3.7].
-  
-  To get started, you might want to try some of these simple commands to
-  understand the search problem that is being passed in:
-  
-  print "Start:", problem.getStartState()
-  print "Is the start a goal?", problem.isGoalState(problem.getStartState())
-  print "Start's successors:", problem.getSuccessors(problem.getStartState())
-  """
-  "*** YOUR CODE HERE ***"
-  util.raiseNotDefined()
+    """
+    Search the deepest nodes in the search tree first.
+    """
+    from util import Stack
+
+    stack = Stack()
+    visited = set()
+    stack.push((problem.getStartState(), []))
+
+    while not stack.isEmpty():
+        state, path = stack.pop()
+
+        if state in visited:
+            continue
+
+        visited.add(state)
+
+        if problem.isGoalState(state):
+            return path
+
+        for successor, action, _ in problem.getSuccessors(state):
+            if successor not in visited:
+                stack.push((successor, path + [action]))
+
+    return []
+
 
 def breadthFirstSearch(problem):
-  """
-  Search the shallowest nodes in the search tree first.
-  [2nd Edition: p 73, 3rd Edition: p 82]
-  """
-  "*** YOUR CODE HERE ***"
-  util.raiseNotDefined()
-      
+    """
+    Search the shallowest nodes in the search tree first.
+    """
+    from util import Queue
+
+    queue = Queue()
+    visited = set()
+    queue.push((problem.getStartState(), []))
+
+    while not queue.isEmpty():
+        state, path = queue.pop()
+
+        if state in visited:
+            continue
+
+        visited.add(state)
+
+        if problem.isGoalState(state):
+            return path
+
+        for successor, action, _ in problem.getSuccessors(state):
+            if successor not in visited:
+                queue.push((successor, path + [action]))
+
+    return []
+
+
 def uniformCostSearch(problem):
-  "Search the node of least total cost first. "
-  "*** YOUR CODE HERE ***"
-  util.raiseNotDefined()
+    """
+    Search the node of least total cost first.
+    """
+    from util import PriorityQueue
+
+    pq = PriorityQueue()
+    visited = set()
+    pq.push((problem.getStartState(), []), 0)
+
+    while not pq.isEmpty():
+        state, path = pq.pop()
+
+        if state in visited:
+            continue
+
+        visited.add(state)
+
+        if problem.isGoalState(state):
+            return path
+
+        for successor, action, cost in problem.getSuccessors(state):
+            if successor not in visited:
+                new_cost = problem.getCostOfActions(path + [action])
+                pq.push((successor, path + [action]), new_cost)
+
+    return []
+
 
 def nullHeuristic(state, problem=None):
-  """
-  A heuristic function estimates the cost from the current state to the nearest
-  goal in the provided SearchProblem.  This heuristic is trivial.
-  """
-  return 0
+    """
+    A heuristic function estimates the cost from the current state to the nearest
+    goal in the provided SearchProblem. This heuristic is trivial.
+    """
+    return 0
+
 
 def aStarSearch(problem, heuristic=nullHeuristic):
-  "Search the node that has the lowest combined cost and heuristic first."
-  "*** YOUR CODE HERE ***"
-  util.raiseNotDefined()
-    
-  
+    """
+    Search the node that has the lowest combined cost and heuristic first.
+    """
+    from util import PriorityQueue
+
+    pq = PriorityQueue()
+    visited = set()
+    pq.push((problem.getStartState(), []), 0)
+
+    while not pq.isEmpty():
+        state, path = pq.pop()
+
+        if state in visited:
+            continue
+
+        visited.add(state)
+
+        if problem.isGoalState(state):
+            return path
+
+        for successor, action, cost in problem.getSuccessors(state):
+            if successor not in visited:
+                new_cost = problem.getCostOfActions(path + [action]) + heuristic(successor, problem)
+                pq.push((successor, path + [action]), new_cost)
+
+    return []
+
+
 # Abbreviations
 bfs = breadthFirstSearch
 dfs = depthFirstSearch
--- searchAgents.py
+++ searchAgents.py
@@ -1,10 +1,4 @@
-# searchAgents.py
-# ---------------
-# Licensing Information: Please do not distribute or publish solutions to this
-# project. You are free to use and extend these projects for educational
-# purposes. The Pacman AI projects were developed at UC Berkeley, primarily by
-# John DeNero (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
+
 
 """
 This file contains all of the agents that can be selected to 
